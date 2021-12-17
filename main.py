825a1b16dc610ab7b8cc833adcde7888805f1af3
unknown <benedikt.pittl@gmx.at>
2021-12-17 18:51:26 +0100

    changed title

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import streamlit as st
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-st.write("# Works2!!")
+st.write("# Works4!!")
 
