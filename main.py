8945569981fc65f1853dfdfd6ed184b11d0b1cc8
unknown <benedikt.pittl@gmx.at>
2021-12-17 16:02:20 +0100

    init

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+import streamlit as st
+
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+st.write("# Works!!")
+
